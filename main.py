6a562279096df6b519e9c4d8d6aec4dd6322a5fa
enyangxxx <ineffab@gmail.com>
2019-08-18 22:30:35 +0200

    add files

--- main.py
+++ main.py
@@ -7,84 +7,38 @@ Created on Tue Aug 13 21:20:14 2019
 """
 
 import os
-import sys
-import scipy.io
-import scipy.misc
-import matplotlib.pyplot as plt
 from matplotlib.pyplot import imshow
-from PIL import Image
-from nst_utils import *
-import numpy as np
+import nst_utils as util
 import tensorflow as tf
-import cv2
-import skimage
+import scipy
+import numpy as np
 
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
 tf.reset_default_graph()
-model = load_vgg_model("imagenet-vgg-verydeep-19.mat")
+model = util.load_vgg_model("imagenet-vgg-verydeep-19.mat")
 
 STYLE_LAYERS = [
-    ('conv1_1', 0.2),
-    ('conv2_1', 0.2),
-    ('conv3_1', 0.2),
-    ('conv4_1', 0.2),
-    ('conv5_1', 0.2)]
+    ('conv1_1', 0.00002),
+    ('conv2_1', 0.00002),
+    ('conv3_1', 0.00002),
+    ('conv4_1', 0.00002),
+    ('conv5_1', 0.00002)]
 
-def compute_style_cost(model, STYLE_LAYERS):
-    """
-    Computes the overall style cost from several chosen layers
-    
-    Arguments:
-    model -- our tensorflow model
-    STYLE_LAYERS -- A python list containing:
-                        - the names of the layers we would like to extract style from
-                        - a coefficient for each of them
-    
-    Returns: 
-    J_style -- tensor representing a scalar value, style cost defined above by equation (2)
-    """
-    
-    # initialize the overall style cost
-    J_style = 0
+content_image = scipy.misc.imread("louvre.jpg")
+#content_image = scipy.misc.imresize(content_image, size=(300,400,3))
+content_image = util.reshape_and_normalize_image(content_image)
 
-    for layer_name, coeff in STYLE_LAYERS:
+style_image = scipy.misc.imread("monet.jpg")
+#style_image = scipy.misc.imresize(style_image, size=(300,400,3))
+style_image = util.reshape_and_normalize_image(style_image)
 
-        # Select the output tensor of the currently selected layer
-        out = model[layer_name]
-        print(out.shape)
-
-        # Set a_S to be the hidden layer activation from the layer we have selected, by running the session on out
-        a_S = sess.run(out)
-
-        # Set a_G to be the hidden layer activation from same layer. Here, a_G references model[layer_name] 
-        # and isn't evaluated yet. Later in the code, we'll assign the image G as the model input, so that
-        # when we run the session, this will be the activations drawn from the appropriate layer, with G as input.
-        a_G = out
-        
-        # Compute style_cost for the current layer
-        J_style_layer = compute_layer_style_cost(a_S, a_G)
-
-        # Add coeff * J_style_layer of this layer to overall style cost
-        J_style += coeff * J_style_layer
-    
-    print("J_style")
-    print(sess.run(J_style))
-    return J_style
-
-
-content_image = plt.imread("content.jpeg")
-content_image = skimage.transform.resize(content_image, output_shape=(300,400,3))
-content_image = reshape_and_normalize_image(content_image)
-
-style_image = plt.imread("monet.jpg")
-style_image = skimage.transform.resize(style_image, output_shape=(300,400,3))
-style_image = reshape_and_normalize_image(style_image)
-
-generated_image = generate_noise_image(content_image)
+generated_image = util.generate_noise_image(content_image)
 imshow(generated_image[0])
 imshow(content_image[0])
 imshow(style_image[0])
+
+np.sum(style_image)
     
 # Assign the content image to be the input of the VGG model.  
 # Start interactive session
@@ -103,14 +57,14 @@ a_C = sess.run(out)
 a_G = out
 
 # Compute the content cost
-J_content = compute_content_cost(a_C, a_G)
+J_content = util.compute_content_cost(a_C, a_G)
 
 # Assign the input of the model to be the "style" image 
 sess.run(model['input'].assign(style_image))
 
 # Compute the style cost
-J_style = compute_style_cost(model, STYLE_LAYERS)
-J = total_cost(J_content, J_style, alpha = 10, beta = 40)
+J_style = util.compute_style_cost(model, STYLE_LAYERS, sess)
+J = util.total_cost(J_content, J_style, alpha = 0.00001, beta = 0.00004)
 
 # define optimizer (1 line)
 optimizer = tf.train.AdamOptimizer(2.0)
@@ -143,10 +97,10 @@ def model_nn(sess, input_image, num_iterations = 10):
             print("style cost = " + str(Js))
             
             # save current generated image in the "/output" directory
-            save_image("out/" + str(i) + ".png", generated_image)
+            util.save_image("out/" + str(i) + ".png", generated_image)
     
     # save last generated image
-    save_image('out/generated_image.jpg', generated_image)
+    util.save_image('out/generated_image.jpg', generated_image)
     
     return generated_image
 
--- nst_utils.py
+++ nst_utils.py
@@ -1,15 +1,6 @@
-### Part of this code is due to the MatConvNet team and is used to load the parameters of the pretrained VGG19 model in the notebook ###
-
-import os
-import sys
 import scipy.io
 import scipy.misc
 import matplotlib.pyplot as plt
-from matplotlib.pyplot import imshow
-from PIL import Image
-from nst_utils import *
-import cv2
-
 import numpy as np
 import tensorflow as tf
 
@@ -17,9 +8,9 @@ class CONFIG:
     IMAGE_WIDTH = 400
     IMAGE_HEIGHT = 300
     COLOR_CHANNELS = 3
-    NOISE_RATIO = 0.6
+    NOISE_RATIO = 0.99
     MEANS = np.array([123.68, 116.779, 103.939]).reshape((1,1,1,3)) 
-    VGG_MODEL = 'pretrained-model/imagenet-vgg-verydeep-19.mat' # Pick the VGG 19-layer model by from the paper "Very Deep Convolutional Networks for Large-Scale Image Recognition".
+    VGG_MODEL = 'imagenet-vgg-verydeep-19.mat' # Pick the VGG 19-layer model by from the paper "Very Deep Convolutional Networks for Large-Scale Image Recognition".
     STYLE_IMAGE = 'images/stone_style.jpg' # Style image to use.
     CONTENT_IMAGE = 'images/content300.jpg' # Content image to use.
     OUTPUT_DIR = 'output/'
@@ -174,7 +165,7 @@ def reshape_and_normalize_image(image):
     image = np.reshape(image, ((1,) + image.shape))
     
     # Substract the mean to match the expected input of VGG16
-    #image = image - CONFIG.MEANS
+    image = image - CONFIG.MEANS
     
     return image
 
@@ -233,8 +224,6 @@ def compute_layer_style_cost(a_S, a_G):
     Returns: 
     J_style_layer -- tensor representing a scalar value, style cost defined above by equation (2)
     """
-    print(a_S.shape)
-    print(a_G.shape)
     
     # Retrieve dimensions from a_G (≈1 line)
     m, n_H, n_W, n_C = a_G.get_shape().as_list()
@@ -265,9 +254,45 @@ def total_cost(J_content, J_style, alpha = 10, beta = 40):
     Returns:
     J -- total cost as defined by the formula above.
     """
-    
-    print(J_content)
-    print(J_style)
+
     J = alpha * J_content + beta * J_style
     
     return J
+
+def compute_style_cost(model, STYLE_LAYERS, sess):
+    """
+    Computes the overall style cost from several chosen layers
+    
+    Arguments:
+    model -- our tensorflow model
+    STYLE_LAYERS -- A python list containing:
+                        - the names of the layers we would like to extract style from
+                        - a coefficient for each of them
+    
+    Returns: 
+    J_style -- tensor representing a scalar value, style cost defined above by equation (2)
+    """
+    
+    # initialize the overall style cost
+    J_style = 0
+
+    for layer_name, coeff in STYLE_LAYERS:
+
+        # Select the output tensor of the currently selected layer
+        out = model[layer_name]
+
+        # Set a_S to be the hidden layer activation from the layer we have selected, by running the session on out
+        a_S = sess.run(out)
+
+        # Set a_G to be the hidden layer activation from same layer. Here, a_G references model[layer_name] 
+        # and isn't evaluated yet. Later in the code, we'll assign the image G as the model input, so that
+        # when we run the session, this will be the activations drawn from the appropriate layer, with G as input.
+        a_G = out
+        
+        # Compute style_cost for the current layer
+        J_style_layer = compute_layer_style_cost(a_S, a_G)
+
+        # Add coeff * J_style_layer of this layer to overall style cost
+        J_style += coeff * J_style_layer
+    
+    return J_style
